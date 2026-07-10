9eb52ccfca71817dbd062a81b31bb5b10774a478
Leonardo C. Surbakti <leonardosurbakti01@gmail.com>
2026-07-10 20:00:10 +0700

    uas leonardos

--- lib/screens/home_tab.dart
+++ lib/screens/home_tab.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../services/api_service.dart';
+import '../services/api_services.dart';
 import 'news_detail.dart';
 
 class HomeTab extends StatefulWidget {
